8482ac55a9f39664ee4182ebd4b7ef5cdfbd3554
ValentinoAbinata <valentabinata@gmail.com>
2025-04-05 20:27:37 +0700

    Changing Project Name

---
+++ android/app/src/main/kotlin/com/example/threee/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.threee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- three/android/app/src/main/kotlin/com/example/three/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.three
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
